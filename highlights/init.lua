f1bc24b293f4d0ae04866b7b48f05b0b56705bbb
Krzysztof Jeziorny <git@jeziorny.net>
2023-09-28 20:39:20 +0200

    URLs in comments

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,5 +1,7 @@
 if vim.g.neovide then
+  -- https://neovide.dev/configuration.html#cursor-settings
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0
+  -- https://neovide.dev/configuration.html#none-at-all
   vim.g.neovide_cursor_vfx_mode = ""
 end
