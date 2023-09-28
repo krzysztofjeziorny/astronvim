27d9f48cf120e6aca25f90812b4cb6c73655d1e3
Krzysztof Jeziorny <git@jeziorny.net>
2023-09-28 20:37:13 +0200

    No cursor animations in Neovide

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,5 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
-}
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_vfx_mode = ""
+end
